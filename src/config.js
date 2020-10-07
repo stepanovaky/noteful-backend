b4752fe55379a608baa8af4fc1b36766723b1440
Katerina <psyckalla.gmail.com>
2020-10-07 18:01:34 -0400

    commit

--- src/config.js
+++ src/config.js
@@ -4,3 +4,8 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://katerina@localhost/noteful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://katerina@localhost/noteful'
   }
+
+export default {
+  API_ENDPOINT: `https://notefulbackend.herokuapp.com/`,
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
