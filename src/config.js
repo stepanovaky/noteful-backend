06f78dbafb96dbbe6c6c3ec8b35ae359e2c4f9db
Katerina <psyckalla.gmail.com>
2020-10-07 20:29:59 -0400

    commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://katerina@localhost/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || process.env.HEROKU_POSTGRESQL_COPPER_URL || 'postgresql://katerina@localhost/noteful' ,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://katerina@localhost/noteful-test'
   }
 
